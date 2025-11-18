68c52053803e5312069e2c3610681f43375609b8
Diego Aguirre <aguirre@embratel.net.br>
2025-11-18 10:09:39 -0300

    Updated electron

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ Constants = {
 	whatsapp: {}
 };
 
-Constants.version = "1.2.0";
+Constants.version = "1.2.5";
 
 Constants.whatsapp.url       = "https://web.whatsapp.com/";
 Constants.whatsapp.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
